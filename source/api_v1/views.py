682591e911f14db7f6de764804e964dc83f69cc0
bak0903 <bak0903@mail.ru>
2019-04-06 14:34:19 +0600

    Добавил представления для сериализаторов

--- source/api_v1/views.py
+++ source/api_v1/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from webapp.models import Good, Category, Order, Photo
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework import viewsets
+from api_v1.serializers import GoodSerializer, CategorySerializer, OrderSerializer
 
-# Create your views here.
+
+class BaseViewSet(viewsets.ModelViewSet):
+    def get_permissions(self):
+        permissions = super().get_permissions()
+
+        if self.request.method in ["POST", "DELETE", "PUT", "PATCH"]:
+            permissions.append(IsAuthenticated())
+        return permissions
+
+
+class GoodViewSet(BaseViewSet):
+    queryset = Good.objects.all()
+    serializer_class = GoodSerializer
+
+
+class CategoryViewSet(BaseViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class OrderViewSet(BaseViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
