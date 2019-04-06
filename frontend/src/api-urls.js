684d70940a55fc4fddcf0dc9251f10b41dc7c0ab
bak0903 <bak0903@mail.ru>
2019-04-06 16:20:52 +0600

    Вынес ссылки для запросов и базовую ссылку axios

--- frontend/src/api-urls.js
+++ frontend/src/api-urls.js
@@ -1,14 +1,12 @@
 import axios from 'axios'
 
 const BASE_URL = 'http://localhost:8000/api/v1';
-const LOGIN_URL = '/login/';
-const HALLS_URL = '/halls/';
-const REGISTER_URL = '/register/';
+const CATALOG = '/goods/';
+
 const instance = axios.create({
     baseURL: BASE_URL
 });
-const TOKEN_LOGIN_URL = '/token-login/';
 
-export {BASE_URL, LOGIN_URL, REGISTER_URL,TOKEN_LOGIN_URL, HALLS_URL}
+export {BASE_URL, CATALOG}
 
 export default instance;
