ffc6fcd95268c80032bc7d01a0d89d9b60e46767
bak0903 <bak0903@mail.ru>
2019-04-06 18:27:00 +0600

    Добавил вывлд цены товара

--- frontend/src/components/GoodCard/GoodCard.js
+++ frontend/src/components/GoodCard/GoodCard.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import img from '../../stub/no_image.png';
-// import {NavLink} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import './GoodCard.css';
 
 
 class GoodCard extends Component {
     render() {
-        const {id, name, photo_good} = this.props.good;
+        const {id, name, photo_good, price} = this.props.good;
         return (
             <div className="card col-sm-12 col-md-4 border-0" id={id}>
                 <div className='photo'>
@@ -14,7 +14,7 @@ class GoodCard extends Component {
                         <img className="card-img-top" src={img} alt="poster"/>}
                 </div>
                     <div className="card-body">
-                        <button><p className="card-text">{name}</p></button>
+                        <NavLink to={'/goods/' + id}><p className="card-text">{name} {price} $</p></NavLink>
                     </div>
             </div>
         );
